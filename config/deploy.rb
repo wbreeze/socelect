6024dc16085f6ab53faa62ec9e3a0821faf72ce2
Douglas Lovell <dcl@dclx61.(none)>
2010-12-26 20:53:57 -0500

    update deployment task

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,15 +14,21 @@ set :deploy_via, :remote_cache
 set :branch, 'master'
 set :scm_verbose, true
 set :use_sudo, false
+set :group_writeable, false
+set :keep_releases, 2
+set :runner, nil
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+   #run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   #from site5 capistrano config:
+   run "cd /home/#{user}; rm -rf public_html; ln -s #{current_path}/public ./public_html"
+   run "skill -9 -u #{user} -c dispatch.fcgi"
+  end
+end
