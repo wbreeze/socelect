26923e9cf03576beb57f4f3d5e3ca62906e9e401
Douglas Lovell <doug@wbreeze.com>
2010-12-26 21:14:48 -0500

    bundle install won't run on shared host

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,9 +34,9 @@ namespace :deploy do
 end
 
 # reconfigure databases
-after "deploy:update_code", :bundle_install
-desc "install the necessary prerequisites"
-task :bundle_install, :roles => :app do
-  run "cd #{release_path} && bundle install"
-end
+# after "deploy:update_code", :bundle_install
+# desc "install the necessary prerequisites"
+# task :bundle_install, :roles => :app do
+#   run "cd #{release_path} && bundle install"
+# end
 
