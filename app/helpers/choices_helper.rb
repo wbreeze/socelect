44b2ed65b5420984ff426838a5932585ba49ce54
Douglas Lovell <doug@wbreeze.com>
2011-02-14 21:16:26 -0500

    prevent remove alternative on choice edit if two or less

--- app/helpers/choices_helper.rb
+++ app/helpers/choices_helper.rb
@@ -7,4 +7,16 @@ def add_alternative_button(name)
    end
 end
 
+def checkAlternativeRemoveJSFunction
+  "var alt = $(this).up('.alternative');" +
+  "if (1 < alt.siblings('.alternative').length)" +
+  "{alt.remove()}" +
+  "else" +
+  "{" +
+  "var ahso = new Element('p',{'class':'fadeMessage'}).update('We need at least two to offer a choice');" +
+  "this.insert({before:ahso});" +
+  "ahso.fade({duration: 2.0, to:0.3}).fade({duration:0.5,from:0.3,queue:'end'});" +
+  "}"
+end
+
 end
