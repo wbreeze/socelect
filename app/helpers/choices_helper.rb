f7bce6a1864561714b1aaf9892494d9aad664127
Douglas Lovell <doug@wbreeze.com>
2011-03-06 16:39:12 -0500

    tabbing of alternatives editing

--- app/helpers/choices_helper.rb
+++ app/helpers/choices_helper.rb
@@ -1,22 +1,2 @@
 module ChoicesHelper
-
-def add_alternative_button(name)
-   button_to_function name do |page|
-      page.insert_html :bottom, :alternatives, :partial => 'alternative', 
-         :object => Alternative.new
-   end
-end
-
-def checkAlternativeRemoveJSFunction
-  "var alt = $(this).up('.alternative');" +
-  "if (1 < alt.siblings('.alternative').length)" +
-  "{alt.remove()}" +
-  "else" +
-  "{" +
-  "var ahso = new Element('p',{'class':'fadeMessage'}).update('We need at least two to offer a choice');" +
-  "this.insert({before:ahso});" +
-  "ahso.fade({duration: 2.0, to:0.3}).fade({duration:0.5,from:0.3,queue:'end'});" +
-  "}"
-end
-
 end
--- public/javascripts/alternatives.js
+++ public/javascripts/alternatives.js
@@ -2,21 +2,15 @@
 $(function()
 {
   var altsContainer = $('#alternatives');
-  //function() {
-    var alts = altsContainer.find('.alternative')
-    var ctAlt = alts.length;
-    if (2 === ctAlt)
-    {
-       alts.find('input[type="button"]').hide();
-    }
-  //}();
   jQuery.extend(altsContainer[0], {
-    addAlternative : function(el)
+    addAlternative: function(el)
     {
       altsContainer.append(el);
       altsContainer.find('input[type="button"]').show();
+      altsContainer.find('.alternative:last input[type="text"]:first').focus();
+      altsContainer[0].renumberTabNavigation();
     },
-    checkRemoveAlternative : function(btn)
+    checkRemoveAlternative: function(btn)
     {
       var alt = $(btn).closest('.alternative'); 
       var sibAlts = alt.siblings('.alternative')
@@ -29,6 +23,38 @@ $(function()
       { 
         sibAlts.find('input[type="button"]').hide();
       }
+    },
+    initializeRemoveButtons: function() {
+      var alts = altsContainer.find('.alternative')
+      var ctAlt = alts.length;
+      if (2 === ctAlt)
+      {
+         alts.find('input[type="button"]').hide();
+      }
+    },
+    renumberTabNavigation: function()
+    {
+      var ti = 1;
+      $form = altsContainer.closest('form');
+      $form.find('input[type="text"],textarea').each(
+        function(index)
+        {
+          $(this).attr('tabIndex', ti++);
+          $(this).attr('tabindex', ti++);
+        }
+      );
+      var addAltTi = ti++;
+      $form.find('input[type="button"][type="submit"]').each(
+        function(index)
+        {
+          $(this).attr('tabIndex', ti++);
+          $(this).attr('tabindex', ti++);
+        }
+      );
+      $form.find('#addAlternative').attr('tabIndex', addAltTi).attr('tabIndex', addAltTi);
     }
   });
+  altsContainer[0].initializeRemoveButtons();
+  altsContainer[0].renumberTabNavigation();
+  altsContainer.find('.alternative:first input[type="text"]:first').focus();
 });
