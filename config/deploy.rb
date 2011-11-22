3f6b6db8b8d97df887c0ede3a2c73bd036ac787e
Douglas Lovell <doug@wbreeze.com>
2011-11-22 16:17:07 -0500

    fix mysql deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :user, 'socelect'
 set :domain, 'socelect.com'
 set :application, 'socelect'
-set :repository,  "#{user}@#{domain}:git/#{application}.git"
+set :repository,  "#{user}@#{domain}:~/git/#{application}.git"
 set :deploy_to, "/home/#{user}/#{application}"
 default_run_options[:pty] = true
 
@@ -15,16 +15,11 @@ set :deploy_via, :remote_cache
 set :branch, 'master'
 set :scm_verbose, true
 set :use_sudo, false
-set :group_writeable, false
-set :keep_releases, 2
-set :runner, nil
 
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
-   run "cd /home/#{user}; rm -rf public_html; ln -s #{current_path}/public ./public_html"
-   # run "skill -9 -u #{user} -c dispatch.fcgi"
    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
@@ -34,6 +29,7 @@ after "deploy:symlink", :enable_passenger
 desc "setup .htaccess for passenger"
 task :enable_passenger, :roles => :app do
   run "echo -e \"PassengerEnabled On\\nPassengerAppRoot #{current_path}\" > #{File.join(current_path, 'public', '.htaccess')}"
+  run "rm -f ~/public_html && ln -s #{File.join(current_path, 'public')} ~/public_html"
 end
 
 after "deploy:update_code", :bundle_install
