f0dd55f506ebd5902779ca64f37790deecd73a10
Douglas Lovell <doug@wbreeze.com>
2019-05-30 15:22:36 -0300

    Replace asset precompilation list

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,9 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w(
+  alternatives.js
+  jquery-1.5.2.min.js
+  jquery-ui-1.8.11.custom.min.js
+  rails.js
+)
