8ba44792f172a8cb2a2191a65599e2cf0c6418a8
Douglas Lovell <doug@wbreeze.com>
2019-06-05 17:16:32 -0300

    Add date time split to choice model

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -2,11 +2,14 @@ require 'validTitleDescription'
 
 class Choice < ApplicationRecord
    include TitleDescriptionValidation
+   attr_accessor :opening_date, :opening_time, :deadline_date, :deadline_time
+
    validate :valid_title_and_description_lengths, :valid_alternative_count
    #apply_simple_captcha
    #validate :is_captcha_valid?, :only => [:new, :edit]
    validates_associated :alternatives
    after_initialize :ensure_two_alternatives, :ensure_default_dates
+   before_validation :resolve_dates
 
    has_many :alternatives, :dependent=>:destroy
    accepts_nested_attributes_for :alternatives, :allow_destroy => true
@@ -37,8 +40,23 @@ class Choice < ApplicationRecord
    end
 
    def ensure_default_dates
-     self.opening = DateTime.current
-     self.deadline = 1.day.from_now
+     self.opening ||= DateTime.current
+     self.opening_date = self.opening.to_date
+     self.opening_time = self.opening.to_time
+     self.deadline ||= 1.day.from_now
+     self.deadline_date = self.deadline.to_date
+     self.deadline_time = self.deadline.to_time
+   end
+
+   def resolve_dates
+     dt = opening_time.to_datetime
+     self.opening = DateTime.new(
+       opening_date.year, opening_date.month, opening_date.day,
+       dt.hour, dt.min, dt.sec, dt.utc_offset)
+     dt = deadline_time.to_datetime
+     self.deadline = DateTime.new(
+       deadline_date.year, deadline_date.month, deadline_date.day,
+       dt.hour, dt.min, dt.sec, dt.utc_offset)
    end
 
    def valid_alternative_count
--- test/models/choice_test.rb
+++ test/models/choice_test.rb
@@ -4,14 +4,12 @@ class ChoiceTest < ActiveSupport::TestCase
   test "validate description no title" do
     desc = 'the description field'
     ch = Choice.new({:title => '', :description => desc})
-    ch.ensureTwoAlternatives
     assert(ch.save)
     assert_equal(desc, ch.title)
   end
 
   test "validate no description or title" do
     ch = Choice.new({:title => '', :description => ''})
-    ch.ensureTwoAlternatives
     refute(ch.save)
   end
 
@@ -20,33 +18,28 @@ class ChoiceTest < ActiveSupport::TestCase
       title: '',
       description: 'This is a not. This is a question?  This is not either.'
     })
-    ch.ensureTwoAlternatives
     assert(ch.save)
     assert_equal('This is a question?', ch.title)
   end
 
   test "validate short title no description" do
     ch = Choice.new({:title => 'short', :description => ''})
-    ch.ensureTwoAlternatives
     refute(ch.save)
   end
 
   test "validate short title some description" do
     ch = Choice.new({:title => 'short', :description => 'some description'})
-    ch.ensureTwoAlternatives
     assert(ch.save)
     assert_equal('short', ch.title);
   end
 
   test "validate short title short description" do
     ch = Choice.new({:title => 'short', :description => 'desc'})
-    ch.ensureTwoAlternatives
     refute(ch.save)
   end
 
   test "validate yes and no alternatives" do
     ch = Choice.new({:title => 'Do you like lobster?', :description => ''})
-    ch.ensureTwoAlternatives
     alts = ch.alternatives
     alts[0].title = 'yes'
     alts[1].title = 'No'
@@ -58,8 +51,52 @@ class ChoiceTest < ActiveSupport::TestCase
 
   test "validate title yes not sufficient" do
     ch = Choice.new({:title => 'yes', :description => ''})
-    ch.ensureTwoAlternatives
     refute(ch.save)
   end
 
+  test 'create has default start and end dates' do
+    now = DateTime.current
+    Timecop.freeze now do
+      ch = Choice.new({title: 'Choice title'})
+      dt_format = '%Y:%m:%d:%H:%M:%S:%z'
+      d_format = '%Y:%m:%d'
+      t_format = '%H:%M:%S:%z'
+      assert_equal(now.strftime(dt_format), ch.opening.strftime(dt_format))
+      assert_equal(ch.opening.strftime(d_format),
+        ch.opening_date.strftime(d_format))
+      assert_equal(ch.opening.to_time.strftime(t_format),
+        ch.opening_time.strftime(t_format))
+      assert(now < ch.deadline)
+      assert(now.to_date < ch.deadline_date)
+      assert_equal(ch.deadline.strftime(d_format),
+        ch.deadline_date.strftime(d_format))
+      assert_equal(ch.deadline.to_time.strftime(t_format),
+        ch.deadline_time.strftime(t_format))
+      assert_equal(now.to_time.strftime(t_format),
+        ch.deadline_time.strftime(t_format))
+    end
+  end
+
+  test 'save writes opening and deadline date and time' do
+    ts = DateTime.current + 1.day
+    te = DateTime.current + 2.days
+    ch = Choice.new({title: 'Choice title'})
+    ch.opening_date = Date.new(ts.year, ts.month, ts.day)
+    ch.opening_time = DateTime.new(ts.year, ts.month, ts.day, ts.hour, ts.minute)
+    ch.deadline_date = Date.new(te.year, te.month, te.day)
+    ch.deadline_time = DateTime.new(te.year, te.month, te.day, te.hour, te.minute)
+    assert(ch.save)
+    id = ch.id
+    ch = Choice.find(id)
+    assert_equal(ts.hour, ch.opening.hour)
+    assert_equal(ts.min, ch.opening.min)
+    assert_equal(ts.day, ch.opening.day)
+    assert_equal(ts.month, ch.opening.month)
+    assert_equal(ts.year, ch.opening.year)
+    assert_equal(te.hour, ch.deadline.hour)
+    assert_equal(te.min, ch.deadline.min)
+    assert_equal(te.day, ch.deadline.day)
+    assert_equal(te.month, ch.deadline.month)
+    assert_equal(te.year, ch.deadline.year)
+  end
 end
