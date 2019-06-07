8e005348503d6fee377aa1b61974e03da58a13fe
Douglas Lovell <doug@wbreeze.com>
2019-06-07 11:01:46 -0300

    Enable popup time selector

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+//= require jquery-1.5.2.min.js
+//= require jquery-ui-1.8.11.custom.min.js
+//= require rails.js
+//= require jquery.ptTimeSelect.js
+//= require_self
+$(document).ready(function() {
+  $('input.time_entry').ptTimeSelect();
+});
