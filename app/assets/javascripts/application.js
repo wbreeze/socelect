8c7a3b8cc138aa63ea7ea8333115712144cb5da1
Douglas Lovell <doug@wbreeze.com>
2019-06-10 13:33:09 -0300

    Upgrade jquery and jquery-ui

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-//= require jquery-1.5.2.min.js
-//= require jquery-ui-1.8.11.custom.min.js
-//= require rails.js
-//= require jquery.ptTimeSelect.js
-//= require alternatives.js
+//= require jquery-3.4.1
+//= require jquery-ui-1.12.1
+//= require jquery.ptTimeSelect
+//= require alternatives
 //= require_self
 $(document).ready(function() {
   $('input.time_entry').ptTimeSelect();
