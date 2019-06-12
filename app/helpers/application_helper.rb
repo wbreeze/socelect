5ec7c60a0b25750a69809a56fb9bceb8fecb0ef1
Douglas Lovell <doug@wbreeze.com>
2019-06-12 13:21:30 -0300

    Put application name in configuration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def set_page_title(title)
+    content_for(:page_title) { title }
+  end
+
+  def site_name
+    Rails.application.credentials.application_name || request.host
+  end
+
+  def home_button
+    label = "#{site_name} home".capitalize
+    button_to(label, root_path, :method => :get)
+  end
 end
