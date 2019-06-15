c9ddab455795b6f9a51299b02c03578ea756b994
Douglas Lovell <doug@wbreeze.com>
2019-06-15 10:09:39 -0300

    Repair link on confirmation page

---
+++ test/system/choice_confirm_test.rb
@@ -0,0 +1,22 @@
+require "application_system_test_case"
+
+class ChoiceConfirmTest < ApplicationSystemTestCase
+  setup do
+    @choice = create_full_choice
+    @choice.save!
+    @alternative = @choice.alternatives.first
+    visit choice_path(@choice.read_token)
+    radio = find(:xpath,
+      "//form/dl[@class='alternatives']//input[@value='#{@alternative.id}']")
+    radio.click
+    click_on "Submit preference"
+  end
+
+  test "confirm shows choice" do
+    assert_text("You selected: #{@alternative.title}")
+  end
+
+  test "confirm shows results link" do
+    assert_link(result_choice_path(@choice.read_token))
+  end
+end
