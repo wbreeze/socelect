b5369987fb19edd54800369eed7bbbc94ceaac8b
Douglas Lovell <doug@wbreeze.com>
2019-06-15 00:03:06 -0300

    Test and correct links on wrap page

---
+++ test/integration/choice_wrap_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class ChoiceWrapTest < ActionDispatch::IntegrationTest
+  setup do
+    @choice = create_full_choice
+    @choice.save!
+    get wrap_choice_path(@choice.edit_token)
+    assert_response :success
+  end
+
+  test "wrap has edit link" do
+    assert_match(edit_choice_path(@choice.edit_token), @response.parsed_body)
+  end
+
+  test "wrap has view link" do
+    assert_match(choice_path(@choice.read_token), @response.parsed_body)
+  end
+
+  test "wrap has result link" do
+    assert_match(result_choice_path(@choice.read_token), @response.parsed_body)
+  end
+end
