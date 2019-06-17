183740690c2791fb82e57210fd26431e88710394
Douglas Lovell <doug@wbreeze.com>
2019-06-17 15:06:31 -0300

    Redirect after choice post

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -22,7 +22,7 @@ class ChoicesController < ApplicationController
   def create
     @choice = Choice.new(choice_params)
     if @choice.save
-      render :wrap
+      redirect_to wrap_choice_path(@choice.edit_token)
     else
       render :new
     end
@@ -33,35 +33,10 @@ class ChoicesController < ApplicationController
     @choice = Choice.find_by(edit_token: params[:id])
     return head :bad_request unless @choice
     if @choice.update_attributes(choice_params)
-      render :wrap
+      redirect_to wrap_choice_path(@choice.edit_token)
     else
       render :edit
     end
-  end
-
-  # POST /choices/:id/selection
-  def selection
-    @choice = Choice.find_by(read_token: params[:id])
-    return head :bad_request unless @choice
-    @alternative = @choice.alternatives.find(params[:alternative])
-    @preference = Preference.new(:choice => @choice)
-    @preference.chef_parameters(request)
-    @expression = @preference.expression.build(:sequence => 1)
-    @expression.alternative = @alternative
-
-    if @preference.save
-      redirect_to :action=> :confirm, :id => @preference.id 
-    else
-      render :action => 'show', :id => @expression.preference.choice
-    end
-  end
-
-  # GET /choices/:id/confirm
-  # :id is Preference id
-  def confirm
-    @preference = Preference.find(params[:id])
-    @choice = @preference.choice
-    @alternative = @preference.expression[0].alternative
   end
 
   # GET /choices/:id/result
