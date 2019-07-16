7eb10f3aea95a55a40e0f14c682367065c6ab4b4
Douglas Lovell <doug@wbreeze.com>
2019-07-16 19:47:47 -0300

    Update choice confirmation system test

--- test/system/choice_confirm_test.rb
+++ test/system/choice_confirm_test.rb
@@ -4,13 +4,15 @@ class ChoiceConfirmTest < ApplicationSystemTestCase
   setup do
     @choice = create_full_choice
     @choice.save!
-    @alternative = @choice.alternatives.first
     visit choice_path(@choice.read_token)
     click_on "Submit preference"
   end
 
   test "confirm shows choice" do
-    assert_text("You selected: #{@alternative.title}")
+    assert_text("You selected:")
+    @choice.alternatives.each do |alt|
+      assert_text(alt.title)
+    end
   end
 
   test "confirm shows results link" do
