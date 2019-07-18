252ee236b52266e0f354e1c5623551fb410b8fe0
Douglas Lovell <doug@wbreeze.com>
2019-07-18 12:34:38 -0300

    Randomize initial order of alternatives

--- app/models/choice/parto_coding.rb
+++ app/models/choice/parto_coding.rb
@@ -1,10 +1,20 @@
 # Use this module to extend a choice with Parto encoding and decoding behavior
 
 module Choice::PartoCoding
-  # Build and retern a JSON object with an array keys and descriptions
-  def parto_encoding
-    alternatives.collect do |alt|
+  # Build and retern a JSON string with an array keys and descriptions
+  def parto_items_encoding
+    items = alternatives.collect do |alt|
       { key: alt.id.to_s, description: alt.title }
     end
+    items.to_json
+  end
+
+  # Build and return a JSON string representing a parto
+  # with items ranked together in random sequence
+  def parto_random_initial
+    keys = alternatives.collect do |alt|
+      alt.id.to_s
+    end
+    [keys.shuffle].to_json
   end
 end
